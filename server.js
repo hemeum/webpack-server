b2b2c3f40b65b480c83e46da95561a9bb063a9b5
hemeum <dnwjddus1506@hanmail.net>
2023-08-23 20:26:50 +0900

    gasg

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path');
 const port = 3000
 
-app.use(express.static('https://hemeum.github.io/webpack-client/')); 
+app.use(express.static('https://hemeum.github.io/webpack-client')); // static은 static한 배포 파일들을 보내줘
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'https://hemeum.github.io/webpack-client/index.html'));
