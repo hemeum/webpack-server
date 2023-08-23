95235d40f81ee5b8683437091fad68e1518f0494
hemeum <dnwjddus1506@hanmail.net>
2023-08-23 22:52:33 +0900

    agsg

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const path = require('path');
 const port = 3000;
 
 // app.use(express.static('https://hemeum.github.io/webpack-client')); // static은 static한 배포 파일들을 보내줘
+// 서버를 사용할 때 프론트 화면은 build까지 하면 되고 서버로 build에 있는걸 배포하는 것.
+// 즉, 프론트를 따로 배포해서 서버로 연결이 들어올 때 배포한 프론트 url을 보내주는 것이 아님!
+// 서버를 배포하면 build에 있는 html 등의 static 파일을 보내주는 것.
+// 배포는 localhost를 도메인으로 만드는 것임!!!!!!!
 
-app.use(express.static('./../webpack/dist'));
+app.use(express.static(path.resolve(__dirname,'./../webpack/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './../webpack', 'dist', 'index.html'));
