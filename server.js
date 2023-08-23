2ea5fd3d295fe31fa342744b91ad58bd79e9628e
hemeum <dnwjddus1506@hanmail.net>
2023-08-23 20:53:41 +0900

    client build 작업물 서버로 띄우기

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const port = 3000
+const port = 3000;
 
-app.use(express.static('https://hemeum.github.io/webpack-client')); // static은 static한 배포 파일들을 보내줘
+// app.use(express.static('https://hemeum.github.io/webpack-client')); // static은 static한 배포 파일들을 보내줘
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'https://hemeum.github.io/webpack-client/index.html'));
-})
+app.use(express.static('./../webpack/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './../webpack', 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`서버 열렸다. ${port}`)
-})
+});
